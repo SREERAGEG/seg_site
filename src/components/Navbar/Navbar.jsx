1fff6cba398e50c89ca8dd3fbce7e4384af0ffa8
SREERAGEG <sreerageg@gmail.com>
2024-04-26 20:58:02 +0530

    Navbar almost

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar =() =>{
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
                 <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} alt="MenuButton" onClick={()=> setMenuOpen(!menuOpen)}/>
-        <ul className={styles.menuItems}>
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={ () => setMenuOpen(false)}>
             <li>
                 <a href='#about'>About</a>
             </li>
