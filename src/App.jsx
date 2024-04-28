b1618a74b9d2607da31918862c1d5dc024fde748
SREERAGEG <sreerageg@gmail.com>
2024-04-28 20:48:53 +0530

    Experience part started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Navbar } from './components/Navbar/Navbar';
 import styles from './App.module.css'
-import { Hero } from './components/Navbar/Hero/Hero';
+import { Hero } from './components/Hero/Hero';
+import { About } from './components/About/About';
+import { Experience } from './components/Experience/Experience';
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <div className={styles.App}>
       <Navbar/>
       <Hero></Hero>
+      <About></About>
+      <Experience></Experience>
     </div>
   );
 }
